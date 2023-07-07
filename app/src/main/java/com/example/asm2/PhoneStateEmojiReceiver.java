42b6457efb7623b39ab426dfeba6140c9443a57d
luongviettri <96829347+luongviettri@users.noreply.github.com>
2023-07-07 09:27:17 +0700

    chuan bi lam broadcast

---
+++ app/src/main/java/com/example/asm2/PhoneStateEmojiReceiver.java
@@ -0,0 +1,12 @@
+package com.example.asm2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PhoneStateEmojiReceiver  extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/com/example/asm2/activity/MainActivity.java
+++ app/src/main/java/com/example/asm2/activity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.asm2.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.asm2.R;
 import com.example.asm2.fragment.FragmentMh1Menu;
@@ -28,12 +34,35 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
 
-
         FragmentMh1Menu fragmentMh1Menu = new FragmentMh1Menu();
 
         getSupportFragmentManager().beginTransaction()
 
                 .replace(R.id.ln_main, fragmentMh1Menu, null).commit();
+
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_CALL_LOG,
+            }, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Quyền đã được cấp
+                Toast.makeText(MainActivity.this, "Quyền đã được cấp", Toast.LENGTH_SHORT).show();
+            } else {
+                // Quyền bị từ chối
+                // Xử lý khi người dùng từ chối cấp quyền
+                Toast.makeText(MainActivity.this, "Please allow this permission to use features of the app", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     /**
